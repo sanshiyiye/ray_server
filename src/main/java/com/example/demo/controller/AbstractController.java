955e265500fc866a039efee56c5226f8c04236ce
wangxiaoguang <xiaoguang.wang@rayjoy.com>
2018-07-11 18:45:41 +0800

    server init

---
+++ src/main/java/com/example/demo/controller/AbstractController.java
@@ -0,0 +1,38 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+@RestController
+public class AbstractController {
+
+//    @Value("${spring.redis.host}")
+    @Value("${spring.redis.host}")
+    private String host;
+    @RequestMapping(value="/doAction",method = RequestMethod.GET)
+    public String doAction(){
+
+        System.out.println(host);
+        return getSession().getId();
+    }
+
+    public static HttpSession getSession() {
+        HttpSession session = null;
+        try {
+            session = getRequest().getSession();
+        } catch (Exception e) {
+        }
+        return session;
+    }
+    public static HttpServletRequest getRequest() {
+        ServletRequestAttributes attrs =(ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        return attrs.getRequest();
+    }
+}
